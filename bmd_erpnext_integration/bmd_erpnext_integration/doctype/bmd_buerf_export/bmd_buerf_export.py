764e539ce507d90fc025410cba4b9df69f5ac198
Roque Vera <roquegv@gmail.com>
2023-08-29 17:12:24 +0000

    use item tax template

--- bmd_erpnext_integration/bmd_erpnext_integration/doctype/bmd_buerf_export/bmd_buerf_export.py
+++ bmd_erpnext_integration/bmd_erpnext_integration/doctype/bmd_buerf_export/bmd_buerf_export.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, Phamos GmbH and contributors
 # For license information, please see license.txt
 
-import frappe
+import frappe, json
 from frappe.model.document import Document
 
 class BMDBuErfExport(Document):
@@ -35,28 +35,52 @@ def get_invoice_list_in_buerf_format(invoice_type, start_date, end_date, only_su
 
 		party_bank_account = frappe.db.get_value(party_name, invoice[party_name.lower()], "default_bank_account")
 		company_bank_account = frappe.db.get_value("Company", invoice["company"], "default_bank_account")
-		steuercode = frappe.db.get_value(tax_table + " Template", invoice["taxes_and_charges"], "steuercode")
+		# steuercode = frappe.db.get_value(tax_table + " Template", invoice["taxes_and_charges"], "steuercode")
 
 		# get tax rate
-		taxes = frappe.db.get_list(tax_table, filters={"parent": invoice["name"]}, fields=["rate"])
-		tax_rate = 0
-		if len(taxes) > 0:
-			tax_rate = taxes[0]["rate"]
+		# taxes = frappe.db.get_list(tax_table, filters={"parent": invoice["name"]}, fields=["rate"])
+		# tax_rate = 0
+		# if len(taxes) > 0:
+		# 	tax_rate = taxes[0]["rate"]
+		taxes = get_taxes(invoice_type, invoice["name"])
 
-		result.append({
-			"satzart": invoice["satzart"],
-			"konto": party_bank_account,
-			"gkonto": company_bank_account,
-			"belegnr": invoice["name"],
-			"belegdatum": invoice["posting_date"],
-			"buchsymbol": buchsymbol,
-			"buchcode": buchcode,
-			"prozent": tax_rate,
-			"steuercode": steuercode,
-			"betrag": invoice["grand_total"],
-			"steuer": invoice["total_taxes_and_charges"],
-			"text": text,
-			"kost": invoice["cost_center"],
-			"extbelegnr": extbelegnr
-		})
+		for tax in taxes:
+			result.append({
+				"satzart": invoice["satzart"],
+				"konto": party_bank_account,
+				"gkonto": company_bank_account,
+				"belegnr": invoice["name"],
+				"belegdatum": invoice["posting_date"],
+				"buchsymbol": buchsymbol,
+				"buchcode": buchcode,
+				"prozent": tax["rate"],
+				"steuercode": tax["tax_code"],
+				"betrag": tax["amount"],
+				"steuer": tax["tax_amount"],
+				"text": text,
+				"kost": invoice["cost_center"],
+				"extbelegnr": extbelegnr
+			})
+	return result
+
+def get_tax_code(doc, item_code):
+	for item in doc.items:
+		if item.item_code == item_code:
+			return frappe.db.get_value("Item Tax Template", item.item_tax_template, "steuercode")
+	return ""
+
+def get_taxes(invoice_type, invoice_name):
+	doc = frappe.get_doc(invoice_type, invoice_name)
+	result = []
+	for tax in doc.taxes:
+		item_wise_tax_detail = json.loads(tax.item_wise_tax_detail)
+		for item_code in item_wise_tax_detail.keys():
+			if item_wise_tax_detail[item_code][1] > 0 and item_wise_tax_detail[item_code][0] > 0:
+				result.append({
+					"rate": item_wise_tax_detail[item_code][0],
+					"tax_amount": round(item_wise_tax_detail[item_code][1], 3),
+					"tax_code": get_tax_code(doc, item_code),
+					"amount": round(item_wise_tax_detail[item_code][1] / (item_wise_tax_detail[item_code][0] / 100), 3)
+				})
+
 	return result
